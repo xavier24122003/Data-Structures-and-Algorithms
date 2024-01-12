60590d53d32ee7d17679856bf29049e5c7efcc4b
Trần Huỳnh Trung Hiếu <132741441+xavier24122003@users.noreply.github.com>
2024-01-12 19:51:28 +0700

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -3,4 +3,7 @@ def fibonacci(n):
     return 1
   return fibonacci(n-1) + fibonacci(n-2)
 
-print(fibonacci(6))
+print(fibonacci(3))
+
+# 3 not in [1,2] : fibonacci(2) + fibonacci(1)
+# 2, 1 in [1,2]: 1 + 1 = 2
