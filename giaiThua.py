1aa3f80af3561babc243f656b4a6e1eb94ff7bf1
Trần Huỳnh Trung Hiếu <132741441+xavier24122003@users.noreply.github.com>
2024-01-12 19:57:22 +0700

    Update giaiThua.py

--- giaiThua.py
+++ giaiThua.py
@@ -1,6 +1,12 @@
 def giaiThua(n):
-  if n == 0:
+  if n == 1:
     return 1
   return giaiThua(n-1) * n
 
 print(giaiThua(5))
+
+# 5 != 0: giaiThua(4) * 5
+# 4 != 0: giaiThua(3) * 4 * 5
+# 3 != 0: giaiThua(2) * 3 * 4 * 5
+# 2!= 0: giaiThua(1) * 2 * 3 * 4 * 5
+# 1 == 1: 1 * 2 * 3 * 4 * 5 = 120
