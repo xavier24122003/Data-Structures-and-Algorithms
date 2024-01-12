a6a5ca75a7acba2691de831392c57e9cccb923cc
Trần Huỳnh Trung Hiếu <132741441+xavier24122003@users.noreply.github.com>
2024-01-12 20:06:29 +0700

    Update powerOfTwo.py

--- powerOfTwo.py
+++ powerOfTwo.py
@@ -3,4 +3,11 @@ def powerOfTwo(n):
     return 1
   return powerOfTwo(n-1) * 2
 
-print(powerOfTwo(10))
+print(powerOfTwo(5))
+
+# 5 != 0: 2 * powerOfTwo(4)
+# 4 != 0: 2 * 2 * powerOfTwo(3)
+# 3 != 0: 2 * 2 * 2 * powerOfTwo(2)
+# 2 != 0: 2 * 2 * 2 * 2 * powerOfTwo(1)
+# 1 != 0: 2 * 2 * 2 * 2 * 2 * powerOfTwo(0)
+# 0 == 0: 2 * 2 * 2 * 2 * 2 * 1 = 32
