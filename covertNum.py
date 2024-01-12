1c46d3b0f34b4cab95877daceced6347f6e2c3c3
Trần Huỳnh Trung Hiếu <132741441+xavier24122003@users.noreply.github.com>
2024-01-12 19:47:57 +0700

    Update covertNum.py

--- covertNum.py
+++ covertNum.py
@@ -4,4 +4,9 @@ def covertDeToBi(n):
     return 0
   return n%2 + 10*covertDeToBi(n//2)
 
-print (covertDeToBi(9))
+print (covertDeToBi(5))
+
+# = 1 + 10 * convertDeToBi(2)
+# = 1 + 10 * (0 + 10 * convertDeToBi(1))
+# = 1 + 10 * (10 * (1 + 10 * convertDeToBi(0))
+# = 1 + 10 * (10 * (1 + 10 * 0))= 101
